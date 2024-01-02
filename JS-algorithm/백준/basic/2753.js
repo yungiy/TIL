244c82aec56efa6992849c5200ed69abaa241723
yungiy <devyungiy@gmail.com>
2024-01-02 18:11:27 +0900

    [update]
    - 알고리즘 준비 백준 기본 풀기

---
+++ JS-algorithm/백준/basic/2753.js
@@ -0,0 +1,3 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+const year = input;
---
+++ JS-algorithm/백준/basic/9498.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+const score = input;
+
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score < 90) {
+  console.log("B");
+} else if (score >= 70 && score < 80) {
+  console.log("C");
+} else if (score >= 60 && score < 70) {
+  console.log("D");
+} else {
+  console.log("F");
+}
