918ac9514ff89b300e31e8ddc77d8e9217bf98d2
양윤기 <138196247+yungiy@users.noreply.github.com>
2024-04-01 23:19:59 +0900

    [update]
    - home, post UI 작성 완료

--- run-nextjs/twitter-clone/src/app/(afterLogin)/_component/Post.tsx
+++ run-nextjs/twitter-clone/src/app/(afterLogin)/_component/Post.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import Link from 'next/link';
+import style from './post.module.css';
+import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
 
 export default function Post() {
+  const target = {
+    User: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+      image: '/yRsRRjGO.jpg',
+    },
+    content: '도지코인 말아먹기 개꿀~~',
+    createdAt: new Date(),
+    Images: [],
+  };
+
   return (
-    <div>Post</div>
-  )
+    <article className={style.post}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            {/* 호버했을 때 포스트 쉐이드로 그림자 생성 */}
+            <div className={style.postShade} />
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={style.postImageSection}></div>
+          <ActionButtons />
+        </div>
+      </div>
+    </article>
+  );
 }
