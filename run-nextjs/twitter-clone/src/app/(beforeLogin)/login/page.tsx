c7867e3798093d5c2b2db456aa4595a5440e1a65
양윤기 <138196247+yungiy@users.noreply.github.com>
2024-04-07 22:06:16 +0900

    [update]
    - useSession() 활용

--- run-nextjs/twitter-clone/src/app/(beforeLogin)/login/page.tsx
+++ run-nextjs/twitter-clone/src/app/(beforeLogin)/login/page.tsx
@@ -2,9 +2,18 @@
 
 import { useRouter } from 'next/navigation';
 import Main from '@/app/(beforeLogin)/_component/Main';
+import { useSession } from 'next-auth/react';
 
 export default function Login() {
   const router = useRouter();
+
+  const session = useSession();
+
+  if (session) {
+    router.replace('home');
+    return null;
+  }
+
   router.replace('/i/flow/login');
   return <Main />;
 }
